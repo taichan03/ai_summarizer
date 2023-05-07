ec634054b5828775d767da132f1c12c15ed7063f
taichan03 <tai.c.chan3@gmail.com>
2023-05-07 18:51:35 -0400

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import {logo} from '../assets';
 
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      
+          <nav className='flex justify-between items-center flex-col'>
+            <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+          </nav>
 
     </header>
   )
