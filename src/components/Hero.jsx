080e2ca04759ba3f29d8ad1b905d7f94aa75f0f9
taichan03 <tai.c.chan3@gmail.com>
2023-05-07 19:08:54 -0400

    Hero heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,26 @@
-import {logo} from '../assets';
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center items-center flex-col'>
-          <nav className='flex justify-between items-center flex-col'>
-            <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-          </nav>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="w-full mb-10 pt-3 flex justify-between items-center flex-row">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
 
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/taichan03")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1>
+        Summarize Articles with
+        <br /> OpenAI GPT-4
+      </h1>
     </header>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
